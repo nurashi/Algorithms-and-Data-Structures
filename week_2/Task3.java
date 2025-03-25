2a6e598b8a6f01c0c554ef0dcd9dfba7d159636a
Nurasyl Orazbek <nur1k.csgo1@gmail.com>
2025-03-25 09:29:55 +0500

    Task 3, The method IsPrime that retuns boolean type, O(n^(1/2)) complexity

---
+++ week_2/Task3.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Task3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); // reading number
+
+        if (isPrime(n)) {
+            System.out.println("Prime");
+        } else {
+            System.out.println("Composite");
+        }
+    }
+
+    /**
+     * This method checks if a number is prime.
+     * It iterates from 2 to sqrt(n) and checks if n is divisible by any number.
+     * If n is divisible by any of them, it will return false.
+     *
+     * Time Complexity: O(n^(1/2)) -> sqrt(n)
+     *
+     * @param n The number to check.
+     * @return boolean True if prime, false otherwise.
+     */
+    static boolean isPrime(int n) {
+        if (n < 2) return false; // 0 and 1 are not prime
+        for (int i = 2; i * i <= n; i++) { // Checking divisibility up to sqrt(n)
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
