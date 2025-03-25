3f6411716daf14992c40bc47247b2f70fa5ddf5c
Nurasyl Orazbek <nur1k.csgo1@gmail.com>
2025-03-25 10:29:27 +0500

    Task 10, gcd of two numbers, method from discrete Math, O(log(min(a,b))) loigarithmic time complexity

---
+++ week_2/Task10.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class GCDRecursive {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        // reading elements
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(gcd(a, b));
+    }
+
+    /**
+     * Recursively calculates the Greatest Common Divisor (GCD) using Euclidean Algorithm.
+     * Formula: GCD(a, b) = GCD(b, a % b)
+     * Base Case: GCD(a, 0) = a
+     *
+     * Time Complexity: O(log(min(a, b))) - Logarithmic time complexity.
+     * Space Complexity: O(log(min(a, b))) - Recursive depth.
+     *
+     * @param a First number
+     * @param b Second number
+     * @return GCD of a and b
+     */
+    static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+}
