99cf38420608aa13a73032ae27ff55348c002753
Nurasyl Orazbek <nur1k.csgo1@gmail.com>
2025-03-25 10:13:44 +0500

    Task 7, print numbers in reverse order, linear time complexity O(n)

---
+++ week_2/Task7.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Task7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt(); // reading numbers count
+
+        reversePrint(n, sc);
+    }
+
+    /**
+     * Recursive function to read and print elements in reverse order.
+     * First, it reads the input, then calls itself recursively before printing.
+     *
+     * @param count Number of elements left to read
+     * @param sc    Scanner object for input
+     */
+    static void reversePrint(int count, Scanner sc) {
+        if (count == 0) return; // input -> from n to n-1 until 0, base case
+
+        int num = sc.nextInt(); // reading current number
+        reversePrint(count - 1, sc);
+        System.out.print(num + " "); // Print after recursion, to print in reverse order
+    }
+}
