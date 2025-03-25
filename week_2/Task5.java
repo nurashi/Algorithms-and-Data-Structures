83e1b401589b1da24ff481d23231c11d8f65fc92
Nurasyl Orazbek <nur1k.csgo1@gmail.com>
2025-03-25 09:35:48 +0500

    Task 5, Time complexity O(2^n) because its repeated function calls

---
+++ week_2/Task5.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Task5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();  // reading number
+        System.out.println(fibonacci(n));
+        sc.close();
+    }
+
+    /**
+     * This recursive method calculates the nth Fibonacci number.
+     * Base cases: If n is 0, return 0; if n is 1, return 1.
+     * Recursive case: F(n) = F(n-1) + F(n-2).
+     *
+     * Time Complexity: O(2^n) due to repeated function calls.
+     *
+     * @param n The Fibonacci sequence index.
+     * @return int The nth Fibonacci number.
+     */
+    static int fibonacci(int n) {
+        if (n == 0) return 0;  // Base case
+        if (n == 1) return 1;  // Base case
+        return fibonacci(n - 1) + fibonacci(n - 2);  // until base case
+    }
+}
