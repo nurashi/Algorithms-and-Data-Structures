aa6215be8bb9117b2fc4616734838c655b3bd1b4
Nurasyl Orazbek <nur1k.csgo1@gmail.com>
2025-03-25 09:25:06 +0500

    Task 2, Avarage of numbers, Linear and Constant time complexity

---
+++ week_2/Task2.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Task2 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // reading the size of the array
+        int n = sc.nextInt();
+
+        if (n <= 0) {
+            System.out.println("size 0, no answer");
+            return;
+        }
+
+
+        int[] arr = new int[n];
+
+        // Reading array elements
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println(Average(arr));
+
+    }
+
+    /**
+     * This method calculates the average of an array of integers.
+     * It iterates through the array using a for loop to sum up all elements.
+     * Then, it divides the sum by the number of elements to get the average.
+     *
+     * Time Complexity: O(n), since it iterates through the array once.
+     *
+     * @param arr The input array of integers.
+     * @return double The calculated average of the array elements.
+     */
+    static double Average(int[] arr) {
+        if(arr.length == 1) {
+            return arr[0]; // Constand time complexity if array consists only one element
+        }
+        int sum = 0;
+        for (int num : arr) {
+            sum += num;
+        }
+        return (double) sum / arr.length; // Casting to double for precise division
+    }
+}
